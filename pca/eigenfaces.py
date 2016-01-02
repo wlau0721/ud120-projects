164ea498bddb5d9080c447c01d5586c1a380df06
wlau0721 <wlau0721@yahoo.com>
2016-01-02 00:03:55 -0800

    submit pca mini project

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -81,6 +81,10 @@ X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
 
+print pca.explained_variance_ratio_
+print "first pc: ", pca.components_[0]
+print "second pc:", pca.components_[1]
+
 
 ###############################################################################
 # Train a SVM classification model
@@ -92,7 +96,8 @@ param_grid = {
           'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
           }
 # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+#clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
 clf = clf.fit(X_train_pca, y_train)
 print "done in %0.3fs" % (time() - t0)
 print "Best estimator found by grid search:"
